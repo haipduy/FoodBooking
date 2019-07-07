a24974330dcf08c8235361204730e248ab0cac97
ngocmy0804 <truongngocmy080496@gmail.com>
2019-07-07 16:31:03 +0700

    Revert "[Create ] MyTN create code"
    This reverts commit 43b5b8086b9845b8b445a2f9087a55edb22c2d78.

--- SourceCode/mobile app/SkypeApp/app/src/main/java/fptu/summer/skypeapp/view/MainActivity.java
+++ SourceCode/mobile app/SkypeApp/app/src/main/java/fptu/summer/skypeapp/view/MainActivity.java
@@ -9,16 +9,10 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.view.MenuItem;
-import android.view.SurfaceView;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -56,27 +50,10 @@ public class MainActivity extends MasterActivity implements SearchView.OnQueryTe
     AccountRoom accRoom;
     ProductPresenter productPresenter;
 
-    SurfaceView surfaceView;
-    CameraSource cameraSource;
-    TextView textView;
-    BarcodeDetector barcodeDetector;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        surfaceView = (SurfaceView)findViewById(R.id.camerapreview);
-        textView = (TextView)findViewById(R.id.scanner);
-
-        barcodeDetector = new BarcodeDetector.Builder(this)
-                .setBarcodeFormats(Barcode.QR_CODE).build();
-
-        cameraSource = new CameraSource.Builder(this,barcodeDetector)
-                .setRequestedPreviewSize(640,480).build();
-
-
-
 
         // ----------------
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
